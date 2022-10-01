5e819013cdbf5483dd1ecbb0a95b4e2fdcaf099c
Julien <julien@ripouteau.com>
2022-10-01 14:05:46 +0200

    feat!: remove react from default package

--- packages/all/index.js
+++ packages/all/index.js
@@ -1,3 +1,3 @@
 module.exports = {
-  extends: ['@julr/eslint-config-vue', '@julr/eslint-config-react', '@julr/eslint-config-prettier'],
+  extends: ['@julr/eslint-config-vue', '@julr/eslint-config-prettier'],
 }
