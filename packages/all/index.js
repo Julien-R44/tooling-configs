0bd0f70951a47f6427644132c7aeb3822685c0d9
Julien <julien@ripouteau.com>
2022-10-08 23:09:05 +0200

    chore(all): use eslint define config

--- packages/all/index.js
+++ packages/all/index.js
@@ -1,3 +1,8 @@
-module.exports = {
+// @ts-check
+/* eslint-disable unicorn/prefer-module */
+
+const { defineConfig } = require('eslint-define-config')
+
+module.exports = defineConfig({
   extends: ['@julr/eslint-config-vue', '@julr/eslint-config-prettier'],
-}
+})
