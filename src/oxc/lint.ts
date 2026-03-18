633c9e2fe1470f73cf2224fcbcbf091dd82f6e41
Julien Ripouteau <julien@ripouteau.com>
2026-03-18 21:55:11 +0100

    fix: move rules to correct `rules` object in adonisjs preset

--- src/oxc/lint.ts
+++ src/oxc/lint.ts
@@ -14,38 +14,38 @@ function adonisjsPreset() {
     'rules': {
       '@adonisjs/prefer-lazy-controller-import': 'error',
       '@adonisjs/prefer-lazy-listener-import': 'error',
-    },
-    'typescript/triple-slash-reference': 'off',
+      'typescript/triple-slash-reference': 'off',
 
-    /**
-     * Knex query builder return a promise but safe to ignore since they are not executed until you call `.exec()` or similar method.
-     */
-    '@typescript-eslint/no-floating-promises': [
-      'error',
-      {
-        allowForKnownSafePromises: [
-          {
-            from: 'package',
-            package: '@adonisjs/lucid',
-            name: [
-              'ExcutableQueryBuilderContract',
-              'ModelQueryBuilderContract',
-              'DatabaseQueryBuilderContract',
-              'InsertQueryBuilderContract',
-              'RawQueryBuilderContract',
-              'ChainableContract',
-              'RelationQueryBuilderContract',
-              'RelationSubQueryBuilderContract',
-              'HasManyQueryBuilderContract',
-              'HasManyThroughQueryBuilderContract',
-              'ManyToManyQueryBuilderContract',
-              'ManyToManySubQueryBuilderContract',
-            ],
-          },
-          { from: 'package', package: 'knex', name: 'QueryBuilder' },
-        ],
-      },
-    ],
+      /**
+       * Knex query builder return a promise but safe to ignore since they are not executed until you call `.exec()` or similar method.
+       */
+      '@typescript-eslint/no-floating-promises': [
+        'error',
+        {
+          allowForKnownSafePromises: [
+            {
+              from: 'package',
+              package: '@adonisjs/lucid',
+              name: [
+                'ExcutableQueryBuilderContract',
+                'ModelQueryBuilderContract',
+                'DatabaseQueryBuilderContract',
+                'InsertQueryBuilderContract',
+                'RawQueryBuilderContract',
+                'ChainableContract',
+                'RelationQueryBuilderContract',
+                'RelationSubQueryBuilderContract',
+                'HasManyQueryBuilderContract',
+                'HasManyThroughQueryBuilderContract',
+                'ManyToManyQueryBuilderContract',
+                'ManyToManySubQueryBuilderContract',
+              ],
+            },
+            { from: 'package', package: 'knex', name: 'QueryBuilder' },
+          ],
+        },
+      ],
+    },
   })
 }
 
